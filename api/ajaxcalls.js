e5ca2182584dbfe2b40cb5f924ea0ffe4128635c
Giuseppe Sollazzo <puntofisso@Giuseppes-MacBook-Pro.local>
2014-09-10 13:58:59 +0100

    Ajax

--- api/ajaxcalls.js
+++ api/ajaxcalls.js
@@ -56,16 +56,16 @@ function showPosition(position) {
 function showPositionError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
+            return "User denied the request for Geolocation."
             break;
         case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
+            return"Location information is unavailable."
             break;
         case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
+            return "The request to get user location timed out."
             break;
         case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
+            return "An unknown error occurred."
             break;
     }
 }
